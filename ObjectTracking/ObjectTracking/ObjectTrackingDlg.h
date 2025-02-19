7fae3f758d6125e678c15f0d9bd5ba31f9ed9881
SangKyoungPark <tltmzh3@gmail.com>
2025-02-19 21:24:19 +0900

    [Add] - .gitignore, .gitattributes files

---
+++ ObjectTracking/ObjectTracking/ObjectTrackingDlg.h
@@ -0,0 +1,34 @@
+
+// ObjectTrackingDlg.h : header file
+//
+
+#pragma once
+
+
+// CObjectTrackingDlg dialog
+class CObjectTrackingDlg : public CDialogEx
+{
+// Construction
+public:
+	CObjectTrackingDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_OBJECTTRACKING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
